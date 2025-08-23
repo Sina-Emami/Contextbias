c2c5f32d3f90d8e453a75a3eae262e45eff8b990
Sina Emami <js.emami99@gmail.com>
2025-08-23 18:18:48 +0200

    add pydantic model for image generator

---
+++ src/schemas/scenario.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class ImageGenerationOutput(BaseModel):
+    id: str
+    image_url: str
+    prompt_used: str
+
+
+class ImageMeta(BaseModel):
+    image_id: str
+    filename: str
+    abspath: str
+    prompt_used: str
+    model: Optional[str] = None
+    seed: Optional[int] = None
