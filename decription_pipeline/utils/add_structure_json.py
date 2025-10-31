178d0964de91b862e39c42ab2dd3d9c778ded56d
Sina Emami <js.emami99@gmail.com>
2025-10-31 13:56:20 +0100

    change model name

--- decription_pipeline/utils/add_structure_json.py
+++ decription_pipeline/utils/add_structure_json.py
@@ -8,7 +8,7 @@ DATASET_ROOT = (Path.cwd() / "dataset")
 if not DATASET_ROOT.exists():
     DATASET_ROOT = Path.cwd()
 
-MODEL_NAME = "Stable-Diffusion-XL"
+MODEL_NAME = "Stable-Diffusion-3.5" # "Stable-Diffusion-XL"
 SUPPORTED_EXTS = {".png", ".jpg", ".jpeg"}  # change if you only want png/jpg
 MANIFEST_FILENAME = "manifest.json"
 # ----------------------------
